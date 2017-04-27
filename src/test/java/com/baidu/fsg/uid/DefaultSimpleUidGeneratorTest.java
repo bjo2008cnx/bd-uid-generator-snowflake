1a188d63cb307bced282c008c24751d901f5b247
xiaoming.wang <18600882246@163.com>
2017-04-27 11:28:31 +0800

    add simple test for debugging

---
+++ src/test/java/com/baidu/fsg/uid/DefaultSimpleUidGeneratorTest.java
@@ -0,0 +1,38 @@
+package com.baidu.fsg.uid;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Test for {@link com.baidu.fsg.uid.impl.DefaultUidGenerator}
+ *
+ * @author yutianbao
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:uid/default-uid-spring.xml"})
+public class DefaultSimpleUidGeneratorTest {
+
+    @Resource
+    private UidGenerator uidGenerator;
+
+    /**
+     * Do generating
+     */
+    @Test
+    public void doGenerate() {
+        long uid = uidGenerator.getUID();
+        String parsedInfo = uidGenerator.parseUID(uid);
+
+        // Check UID is positive, and can be parsed
+        Assert.assertTrue(uid > 0L);
+        Assert.assertTrue(StringUtils.isNotBlank(parsedInfo));
+
+        System.out.println(Thread.currentThread().getName() + " No.  >>> " + parsedInfo);
+    }
+}
